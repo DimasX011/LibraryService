0fc3fbe6c93e9ca8c79b9a25d54676f5f8188aec
Dmitry Yakovlev <dimon1998daf@mail.ru>
2022-12-18 11:36:17 +0600

    Lesson4

---
+++ Lesson4/ClinicService.Data/Client.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClinicService.Data
+{
+    [Table("Clients")]
+    public class Client
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column]
+        [StringLength(50)]
+        public string? Document { get; set; }
+
+
+        [Column]
+        [StringLength(255)]
+        public string? Surname { get; set; }
+
+        [Column]
+        [StringLength(255)]
+        public string? FirstName { get; set; }
+
+        [Column]
+        [StringLength(255)]
+        public string? Patronymic { get; set;}
+
+        [InverseProperty(nameof(Pet.client))]
+        public ICollection<Pet> Pets { get; set; } = new HashSet<Pet>();
+    }
+}
